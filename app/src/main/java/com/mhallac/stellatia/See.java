93c946af99592d7c391725ec561f634be2dcd1f0
Matthew Hallac <matthew.hallac@berkeley.edu>
2019-09-07 22:05:03 -0400

    made progress on camera

--- app/src/main/java/com/mhallac/stellatia/See.java
+++ app/src/main/java/com/mhallac/stellatia/See.java
@@ -2,16 +2,73 @@ package com.mhallac.stellatia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class See extends AppCompatActivity {
-
+    private Button btnCapture;
+    private ImageView imgCapture;
+    private static final int Image_Capture_Code = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Intent intent = getIntent();
         String value = intent.getStringExtra("key");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_see);
+        btnCapture =(Button)findViewById(R.id.btnTakePicture);
+        imgCapture = (ImageView) findViewById(R.id.capturedImage);
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cInt = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cInt,Image_Capture_Code);
+            }
+        });
     }
+
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            if (requestCode == Image_Capture_Code) {
+                if (resultCode == RESULT_OK) {
+                    Bitmap bp = (Bitmap) data.getExtras().get("data");
+                    imgCapture.setImageBitmap(bp);
+                } else if (resultCode == RESULT_CANCELED) {
+                    Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
 }
